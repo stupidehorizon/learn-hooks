646bff4f433682bfdf8725033861f49f04448223
stupidehorizon <122505231@qq.com>
2019-05-23 17:23:56 +0800

    feat: 4.basic use of use useMemo

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React, { Fragment, useState, useEffect, useContext } from 'react';
+import React, { Fragment, useState, useEffect, useMemo } from 'react';
 import ColorBox from './color-box';
 import ColorContext from './color-context';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [divCount, setDivCount] = useState(1);
 
   useEffect(() => {
     document.title = count;
   }, [count]);
 
+  const squareCount = useMemo(() => {
+    console.log('in function');
+    return count**count;
+  }, [count]);
+
+
   return (
     <Fragment>
-      <h1>{count}</h1>
+      <h1>{count} ** {count} = {squareCount}</h1>
       <button onClick={() => setCount(count + 1)} >+</button>
       <button onClick={() => setCount(count - 1)} >-</button>
+      <br />
+      <button onClick={() => setDivCount(divCount + 1)} >+</button>
+      <button onClick={() => setDivCount(divCount - 1)} >-</button>
       <ColorContext.Provider value={count %2 ? 'red' : 'blue'}>
-        <ColorBox />
+        {
+          Array.from({length: divCount}).map(() => <ColorBox />)
+        }
       </ColorContext.Provider>
     </Fragment>
   );
